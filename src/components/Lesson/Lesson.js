d73884b2eabe1db3409bdcef4d1f6a11db6d2fad
У М <umalat.magma@gmail.com>
2020-12-12 05:48:11 +0300

    Перевёл Lesson в grid

--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -6,8 +6,8 @@ const COLORS= {
   "1":'#32BE32',//'#00FF00',
   "2":'#CDC236',//'#40E0D0',
   "3":'#CD4036',//'#0FC0FC',
-  "4":'#BE31B7',//'#BA55D3',
-  "5":'#20319D',//'#008080',
+  "4":'#70CE9B',//'#BA55D3',
+  "5":'#937ACC',//'#008080',
   "6":'#00BE96',//'#D40000',
 }
 
@@ -23,32 +23,25 @@ const Lesson = (props) =>{
   :lesson= props.lesson[keys[1]];
 
   return (
-    <Div
-      //size="l" 
-      className={classes.Lesson}
-      // asideContent={length!==1 
-      //   ? <div className={classes.Aside}>
-      //       <button onClick={()=>{ setCurLesson('left') } } >
-      //           <Icon24ChevronLeft width={12} height={12}/>
-      //       </button>
-      //       <button onClick={()=>{ setCurLesson('right') } } style={{ marginLeft: "8px" }}>
-      //         <Icon24ChevronRight width={12} height={12}/>
-      //       </button>
-      //     </div>
-      //   : null
-      // }
-    >
-      <span className={classes.Aud} >{lesson.aud}</span>
-  
-      <span className={classes.Type}>{lesson.type}</span>
-      <span className={classes.Start}>{lesson.start}</span>
-      <span className={classes.End}>{lesson.end}</span>
-      <span className={classes.Name}>{lesson.name}</span>
-      <span className={classes.Teacher}>{lesson.teacher}</span>
+    <Div className={classes.Lesson}>
       <div className={classes.Rect} style={{backgroundColor: COLORS[lesson.number]}}>
         <span className={classes.Number}>{lesson.number}</span>
       </div>
-      <div className={classes.Line} style={{backgroundColor: COLORS[lesson.number]}}></div>  
+      
+      <span className={classes.Type}>{lesson.type}</span>
+
+      <span className={classes.Aud} >{lesson.aud}</span>
+
+      <div className={classes.Description}>
+        <span className={classes.Name}>{lesson.name}</span>
+        <span className={classes.Teacher}>{lesson.teacher}</span>
+      </div>
+
+      <div className={classes.Time}>
+        <span className={classes.Start}>{lesson.start}</span>
+        <div className={classes.Line} style={{backgroundColor: COLORS[lesson.number]}}></div>
+        <span className={classes.End}>{lesson.end}</span>
+      </div>  
     </Div>
   )
 
