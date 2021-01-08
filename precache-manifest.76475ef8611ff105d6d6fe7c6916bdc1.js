2076ebd163574453b5b64ed653e9039144afde6d
У М <umalat.magma@gmail.com>
2021-01-08 13:09:09 +0300

    Updates

---
+++ precache-manifest.76475ef8611ff105d6d6fe7c6916bdc1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20bf634c24012a6f25441b3ee960288",
+    "url": "/donstu-schedule/index.html"
+  },
+  {
+    "revision": "b44a95d01f2be771e5b7",
+    "url": "/donstu-schedule/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "0b7f4f77cb2e4145ee28",
+    "url": "/donstu-schedule/static/css/main.9dd6e720.chunk.css"
+  },
+  {
+    "revision": "b44a95d01f2be771e5b7",
+    "url": "/donstu-schedule/static/js/2.61ce7102.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "/donstu-schedule/static/js/2.61ce7102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7f4f77cb2e4145ee28",
+    "url": "/donstu-schedule/static/js/main.6deb5700.chunk.js"
+  },
+  {
+    "revision": "e905c16dbbcdd4899268",
+    "url": "/donstu-schedule/static/js/runtime-main.734a43a0.js"
+  },
+  {
+    "revision": "f7f6ba903415440f10371b6a557084bb",
+    "url": "/donstu-schedule/static/media/empty.f7f6ba90.png"
+  },
+  {
+    "revision": "05bcf8a77c774e76131912cb56772885",
+    "url": "/donstu-schedule/static/media/logo.05bcf8a7.png"
+  }
+]);
