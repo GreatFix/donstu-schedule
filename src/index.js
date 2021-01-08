550297b4c8d02c457399f0e09e38570c5b2fb502
У М <umalat.magma@gmail.com>
2021-01-08 13:50:29 +0300

    Очистил package.json от неиспользуемых библиотек

--- src/index.js
+++ src/index.js
@@ -48,10 +48,6 @@ const appInit = async () => {
     </Provider>,
     document.getElementById('root')
   )
-
-  if (process.env.NODE_ENV === 'development') {
-    import('./eruda').then(({ default: eruda }) => {}) //runtime download
-  }
 }
 
 appInit()
